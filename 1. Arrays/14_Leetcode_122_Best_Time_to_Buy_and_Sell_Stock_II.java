713ac562d914a38ef9ad1131b652a743bcaa5db7
Saransh Sharma <sarsats1@gmail.com>
2022-02-01 21:16:44 +0530

    Create 14_Leetcode_122_Best_Time_to_Buy_and_Sell_Stock_II.java

---
+++ 1. Arrays/14_Leetcode_122_Best_Time_to_Buy_and_Sell_Stock_II.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int bd =0;//buy date
+        int sd = 0;//sell date
+        int profit = 0;
+        
+        for(int i=1; i<prices.length; i++){
+            
+            if(prices[i]>prices[i-1]){
+                
+                sd++;
+            }else{
+                profit += prices[sd] - prices[bd];
+                bd =sd= i;
+            }
+        }
+        
+        profit += prices[sd] - prices[bd];
+        
+        return profit;
+    }
+}
