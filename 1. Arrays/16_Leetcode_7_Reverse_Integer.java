4d776e914c8435c41ab61752304bfb973b88384b
Saransh Sharma <sarsats1@gmail.com>
2022-02-02 20:13:29 +0530

    Create 16_Leetcode_7_Reverse_Integer.java

---
+++ 1. Arrays/16_Leetcode_7_Reverse_Integer.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int reverse(int x) {
+        
+        if(x==0)
+            return 0;
+        long reverse=0;
+        while(x!=0)
+        {
+            int last_digit = x%10;
+            reverse = reverse*10+last_digit;
+            x=x/10;
+        }
+        
+        
+        
+        if(reverse > Integer.MAX_VALUE || reverse < Integer.MIN_VALUE) {
+        return 0;
+        }
+        else
+            // System.out.println((int)reverse);
+            return (int)reverse;
+    }
+}
