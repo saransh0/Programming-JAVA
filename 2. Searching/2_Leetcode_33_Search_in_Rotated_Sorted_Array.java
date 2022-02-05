04fd276287566475a170264a1cf7dfbeca3fb882
Saransh Sharma <sarsats1@gmail.com>
2022-02-05 07:17:23 +0530

    Create 2_Leetcode_33_Search_in_Rotated_Sorted_Array.java

---
+++ 2. Searching/2_Leetcode_33_Search_in_Rotated_Sorted_Array.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int search(int[] arr, int key) {
+        
+        int low =0;
+        int high = arr.length-1;
+        
+        while(low<=high){
+            
+            int mid = low + (high-low)/2;
+            
+            if(arr[mid]==key){
+                return mid;
+            }
+            else if(arr[low]<=arr[mid]){
+                
+                if(key>=arr[low] && key<=arr[mid])
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }
+            else if(arr[mid]<=arr[high]){
+                
+                if(key>=arr[mid] && key<=arr[high])
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+        }
+        
+        return -1;
+    }
+}
